e76ad614d4d67ffab1e54ed3920606bdbaf8fdfd
BUKKE sai  naik <159629643+sainaik9985@users.noreply.github.com>
2024-05-22 21:52:59 +0530

    created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function Hello() {
-  return (
-    <div>
-      <h1>hai this is sainaik wel come to my react webpage</h1>
+  // const employe={
+  //   name: "10kcooders"
+  // }
+  // return (
+  //   <div>
+  //     <h1>hai this is sainaik wel come to my react webpage{employe.name}</h1>
       
-    </div>
-  );
+  //   </div>
+  // );
+
+  const isSubscribed=true
+  if(isSubscribed){
+    return <h1>thanks for subscribe</h1>
+  }else{
+    return <h1>please subscribe </h1>
+  }
 }
 export default Hello;
